e64ca6fd8eba857f08d6159808e9465c09c9a3c8
Olga Buyvolova <o.buyvolova@mail.ru>
2023-04-07 17:22:37 +0300

    Stub ad tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,6 @@
 public class PhoneBook {
+
+    public static int add(String name, String num) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd() {
+        int result = PhoneBook.add("Masha", "89261111111") +
+                PhoneBook.add("Olya", "89261111112");
+        assertEquals(result, 3);
+    }
+
+    @Test
+    public void testAddName() {
+        int result = PhoneBook.add("Masha", "89261111111") +
+                PhoneBook.add("Olya", "89261111112") + PhoneBook.add("Olya", "89261111112");
+        assertEquals(result, 5);
+    }
+
+
 }
