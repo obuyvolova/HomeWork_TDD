fed45c22b994ec33ab89c0a910900baacd99cbe1
Olga Buyvolova <o.buyvolova@mail.ru>
2023-04-07 17:41:18 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,13 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+    static Map<String, String> phoneBook = new TreeMap<>();
 
     public static int add(String name, String num) {
-        return 0;
+        if (!phoneBook.containsKey(name)) {
+            phoneBook.put(name, num);
+        }
+        return phoneBook.size();
     }
 }
